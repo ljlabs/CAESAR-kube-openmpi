9e5107f458503b11e5194cde1ea69b0b1f37e34e
Kyle Jordaan <kyle.jordaan@takealot.com>
2020-07-03 15:00:59 +0200

    added persitant storage

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 export KUBE_NAMESPACE="default"
 export MPI_CLUSTER_NAME="caesar"
 ./gen-ssh-key.sh
-helm template chart --namespace $KUBE_NAMESPACE --name-template $MPI_CLUSTER_NAME -f values.yaml -f ssh-key.yaml | kubectl -n $KUBE_NAMESPACE delete -f -
-helm template chart --namespace $KUBE_NAMESPACE  --name-template $MPI_CLUSTER_NAME -f values.yaml -f ssh-key.yaml | kubectl -n $KUBE_NAMESPACE create -f -
+
+kubectl delete statefulset.apps/caesar-openmpi-laptop-worker service/caesar-openmpi-laptop pod/caesar-openmpi-laptop-master && kubectl delete pvc  kubectl delete pvc scratch-claim
+
+# helm template chart --namespace $KUBE_NAMESPACE --name-template $MPI_CLUSTER_NAME -f values.yaml -f ssh-key.yaml | kubectl -n $KUBE_NAMESPACE delete -f -
+# helm template chart --namespace $KUBE_NAMESPACE  --name-template $MPI_CLUSTER_NAME -f values.yaml -f ssh-key.yaml | kubectl -n $KUBE_NAMESPACE create -f -
+
+# mpirun --allow-run-as-root -x LD_LIBRARY_PATH -machinefile ./hostfile -np 16 /opt/caesar/bin/FindSourceMPI -c config.cfg 
